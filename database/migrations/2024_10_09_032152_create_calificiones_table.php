9b0fbc107d903de63432eb4338cfbe8c6a3ab35c
Lo32nz1 <reloren1918@gmail.com>
2024-10-09 00:44:55 -0300

    add: migración calificaciones y tipo calificaciones

---
+++ database/migrations/2024_10_09_032152_create_calificiones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tipo_calificiones', function (Blueprint $table) {
+            $table->id('id_tipo_calificaciones');
+            $table->integer('cantidad estrellas');
+        });
+
+        Schema::create('calificiones', function (Blueprint $table) {
+            $table->id('id_calificaciones');
+            $table->foreignId('id_estacion')->constrained('estaciones', 'id_estacion')->onDelete('cascade');
+            $table->foreignId('id_tipo_calificaciones')->constrained('tipo_calificaciones', 'id_tipo_calificaciones')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calificiones');
+        Schema::dropIfExists('tipo_calificiones');
+    }
+};
