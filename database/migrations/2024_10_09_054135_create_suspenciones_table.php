43f014c8b286a89f12f8e0ade7fdcd99c5f7f8e4
Lo32nz1 <reloren1918@gmail.com>
2024-10-09 03:12:58 -0300

    add: migración de suspensión y tipo suspensión

---
+++ database/migrations/2024_10_09_054135_create_suspenciones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tipo_suspensiones', function (Blueprint $table) {
+            $table->id('id_estado_suspensiones');
+            $table->string('nombre');
+        });
+
+        Schema::create('suspensiones', function (Blueprint $table) {
+            $table->id('id_suspensiones');
+            $table->foreignId('id_usuario')->constrained('clientes', 'id_usuario')->onDelete('cascade');
+            $table->date('fecha_desde');
+            $table->date('fecha_hasta');
+            $table->string('descripcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suspensiones');
+        Schema::dropIfExists('tipo_suspensiones');
+    }
+};
