94ef19ba02f8ccfdfbfe780734b79e81d9c8766c
Daria Sauco <dsauco36@gmail.com>
2024-10-17 19:20:22 -0300

    fix: volví a agregar la config de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,28 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                "pc-rojo": "#cd3f4b",
+                "pc-naranja": "#eb7c6c",
+                "pc-azul": "#0097a7",
+                "pc-celeste": "#66bba8",
+                "pc-texto-h": "#302929",
+                "pc-texto-p": "#413c3c",
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans], //Esto estaba en el merge de iniciar sesión, asique lo dejé
+            },
+        },
+        fontFamily: {
+            Montserrat: ['Montserrat', 'sans-serif']
+        },
+        container: {
+            center: true,
+            padding: "1rem",
+            screens: {
+                lg: "1124px",
+                xl: "1124px",
+                "2xl": "1124px",
             },
         },
     },
