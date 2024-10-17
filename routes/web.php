0c6d2e1c6bccb1b45795e6bd56bbd97373a3ffbb
Maxi Mansilla <maximansilla902@gmail.com>
2024-10-17 00:24:53 -0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Esta se puede sacar
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Ruta para el dashboard de usuario
+Route::middleware(['auth', 'role:cliente'])->group(function () {
+    Route::get('/cliente/dashboard', function () {
+        return view('cliente.dashboard'); // Vista para el usuario
+    })->name('cliente.dashboard');
+});
+
+// Ruta para el dashboard de administrador
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard'); // Vista para el administrador
+    })->name('admin.dashboard');
+});
+
+// Route::get('/', function () {
+//     return view('welcome'); // Vista por defecto
+// })->name('home');
+
 Route::get('/', function () {
-    return view('invitado.landing');
+    return view('invitado.landing')
+    ->name('home');
 });
 
 Route::get('/alquilar', function () {
@@ -17,3 +48,6 @@ Route::get('/devolver', function () {
 Route::get('/reservar', function () {
     return view('cliente.reservar');  // Renderiza la vista 'home.blade.php'
 });
+
+
+require __DIR__.'/auth.php';
