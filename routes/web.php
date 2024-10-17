9507bd791bdd44b35f083822359ecf3a8a94ebde
Your Name <your-email@example.com>
2024-10-16 22:41:19 -0300

    route alquilar added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('invitado.landing');
 });
+
+Route::get('/alquilar', function () {
+    return view('cliente.alquilar');  // Renderiza la vista 'home.blade.php'
+});
