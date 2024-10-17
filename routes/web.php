d931af345bc15dac5271998ff9726f3dd043036c
Your Name <your-email@example.com>
2024-10-16 22:51:42 -0300

    daria´s view routes, added

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/alquilar', function () {
     return view('cliente.alquilar');  // Renderiza la vista 'home.blade.php'
 });
+
+Route::get('/devolver', function () {
+    return view('cliente.devolver');  // Renderiza la vista 'home.blade.php'
+});
+
+Route::get('/reservar', function () {
+    return view('cliente.reservar');  // Renderiza la vista 'home.blade.php'
+});
