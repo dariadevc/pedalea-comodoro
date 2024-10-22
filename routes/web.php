123eb0d520090108b13fe33f0f0d6bb644b97d58
Daria Sauco <dsauco36@gmail.com>
2024-10-21 23:27:45 -0300

    fix: vista de inicio según el tipo de usuario

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\EstacionController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 // Vista principal
 Route::get('/', function () {
@@ -12,15 +14,15 @@ Route::get('/', function () {
 
 
 //? Cuando lo meto en el grupo de rutas para invitados no me permite entrar a esas vistas, ¿no me reconoce como invitado?
-// // Grupo de rutas para invitados
-// Route::middleware('guest')->group(function () {
-//     Route::get('/iniciar-sesion', function () {
-//         return view('invitado.iniciar_sesion'); // Vista para iniciar sesión
-//     })->name('iniciar_sesion');
-//     Route::get('/registrarse', function () {
-//         return view('invitado.registrarse'); // Vista para registrarse
-//     })->name('registrarse');
-// });
+// Grupo de rutas para invitados
+Route::middleware('guest')->group(function () {
+    Route::get('/iniciar-sesion', function () {
+        return view('invitado.iniciar_sesion'); // Vista para iniciar sesión
+    })->name('iniciar_sesion');
+    Route::get('/registrarse', function () {
+        return view('invitado.registrarse'); // Vista para registrarse
+    })->name('registrarse');
+});
 
 
 // Esta se puede sacar
@@ -34,35 +36,56 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-Route::middleware(['auth', 'role:administrativo'])->group(function () {
-    Route::get('/admin-inicio', function () {
-        return view('administrativo.inicio'); // Vista para el administrador
-    })->name('administrativo.inicio');
-});
-
-Route::middleware(['auth', 'role:cliente'])->group(function () {
+// Ruta para redirigir según el rol
+Route::middleware('auth')->group(function () {
     Route::get('/inicio', function () {
-        return view('cliente.inicio'); // Vista para el usuario
-    })->name('cliente.inicio');
+        $user = Auth::user();
+
+        if ($user->hasRole('cliente')) {
+            return view('cliente.inicio');
+        } elseif ($user->hasRole('administrativo')) {
+            return view('administrativo.inicio');
+        } elseif ($user->hasRole('inspector')) {
+            return view('inspector.inicio');
+        }
+
+        // Si no tiene rol, lo manda al landing
+        return redirect()->route('landing');
+    })->name('inicio');
+});
+
+// Vistas del administrativo
+// Route::middleware(['auth', 'role:inspector'])->group(function () {
+
+// });
+
+
+// Vistas del inspector
+// Route::middleware(['auth', 'role:inspector'])->group(function () {
+
+// });
+
+// Vistas del cliente
+Route::middleware(['auth', 'role:cliente'])->group(function () {
+
+    Route::get('/alquilar', function () {
+        return view('cliente.alquilar');  // Renderiza la vista 'home.blade.php'
+    })->name('alquiler');
+
+    Route::get('/devolver', function () {
+        return view('cliente.devolver');  // Renderiza la vista 'home.blade.php'
+    });
+
+    Route::get('/reservar', function () {
+        return view('cliente.reservar');  // Renderiza la vista 'home.blade.php'
+    });
 });
 
 
 
 
 
-Route::get('/alquilar', function () {
-    return view('cliente.alquilar');  // Renderiza la vista 'home.blade.php'
-})->name('alquiler');
 
-Route::get('/devolver', function () {
-    return view('cliente.devolver');  // Renderiza la vista 'home.blade.php'
-});
-
-Route::get('/reservar', function () {
-    return view('cliente.reservar');  // Renderiza la vista 'home.blade.php'
-});
 
 
 // Ruta para obtener estaciones, protegida por autenticación y CSRF
