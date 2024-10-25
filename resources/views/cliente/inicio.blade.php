125140b8369636a33d8827f879803240808a7d0a
Maxi Mansilla <maximansilla902@gmail.com>
2024-10-25 00:45:51 -0300

    comentario

--- resources/views/cliente/inicio.blade.php
+++ resources/views/cliente/inicio.blade.php
@@ -11,17 +11,21 @@
         <img src="img/bicicleta.png" alt="" class="h-14 w-14">
         <p class="text-xl font-semibold text-pc-texto-h">¡Hola, <span class="font-bold text-pc-rojo">
                 {{ Auth::user()->nombre }}</span>!</p>
+
+        {{-- ALERTA DE EXITO --}}
         @if (session('success'))
             <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
                 <span class="block sm:inline">{{ session('success') }}</span>
             </div>
         @endif
 
+        {{-- ALERTA DE ERROR --}}
         @if (session('error'))
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                 <span class="block sm:inline">{{ session('error') }}</span>
             </div>
         @endif
+        
     </div>
     {{-- TARJETA RESERVA/ALQUILER ACTUAL --}}
     {{-- * La información de esta tarjeta se actualiza, si no tiene reserva lo va a mandar a reservar, si tiene reserva en curso muestra algunos datos y te manda a consultar reserva, si tiene alquiler en curso muestra algunos datos y te manda a consultar alquiler * --}}
