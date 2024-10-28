c1d1dce75980932fb0fe4c198530e52e1ff9acf3
Daria Sauco <dsauco36@gmail.com>
2024-10-27 22:58:38 -0300

    add: jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import $ from 'jquery';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+window.$ = window.jQuery = $;
+
+$(document).ready(function() {
+    console.log('jQuery is working!');
+});
