cef233428f7d81377bd6064bd98c53958a089b03
Joako23L <joako_23402@hotmail.com>
2024-10-29 17:11:17 -0300

    Add: Se agrego el metodo informe_rutas dentro del controlador de informes para llevar a cabo la funcionalidad de poder obtener las rutas mas utilizadas por los clientes, en base a cual combinacion de estaciones fue mas utilizada(retiro->devolucion).

--- app/Http/Controllers/InformeController.php
+++ app/Http/Controllers/InformeController.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\Constraint\Count;
 
 class InformeController extends Controller
 {
-    public function multas(Request $request)
+    public function informe_multas(Request $request)
     {
         $fechaInicio = $request->input('fecha_inicio') . ' 00:00:00';  //Pongo de esta forma la hora para que me tome todo el dia
         $fechaFin = $request->input('fecha_fin') . ' 23:59:59';        //Si no ponia la misma fecha de inicio y de fin y no me filtraba por el cambio de horario
@@ -31,7 +31,7 @@ class InformeController extends Controller
 
 //De la otra forma con la lista y el validatte, tendria que probar con date_format, no con date solo.
 
-    public function estaciones (Request $request)
+    public function informe_estaciones (Request $request)
     {
         $fechaInicio = $request->input('fecha_inicio') . ' 00:00:00';
         $fechaFin = $request->input('fecha_fin') . ' 23:59:59'; 
@@ -41,9 +41,10 @@ class InformeController extends Controller
             $estaciones = DB::table('reservas')
             ->select(DB::raw('id_estacion_retiro, estaciones.nombre, COUNT(*) as total_reservas')) //DB::RAW sirve para utilizar funciones SQL que laravel no tiene
             ->join('estaciones', 'reservas.id_estacion_retiro' ,'=', 'estaciones.id_estacion')
-            ->whereBetween(DB::raw('DATE(fecha_hora_retiro)'), [$fechaInicio, $fechaFin])
+            ->whereBetween(DB::raw('DATE(fecha_hora_devolucion)'), [$fechaInicio, $fechaFin])
             ->groupBy('id_estacion_retiro', 'nombre')
             ->orderBy('total_reservas', 'desc')
+            ->where('reservas.id_estado', 3)
             ->get();
 
         }else{
@@ -51,6 +52,31 @@ class InformeController extends Controller
         }
         return view('informes.estacionesInforme', compact('estaciones'));
 
+    }
+
+    public function informe_rutas (Request $request)
+    {
+        $fechaInicio = $request->input('fecha_inicio') . ' 00:00:00';
+        $fechaFin = $request->input('fecha_fin') . ' 23:59:59'; 
+
+        if ($fechaInicio && $fechaFin)
+        {
+            $rutas = DB::table('reservas')
+            ->select(DB::raw('CONCAT(reservas.id_estacion_retiro, " -> ", reservas.id_estacion_devolucion) as rutas, CONCAT(r.nombre, " -> " ,d.nombre) as nombresEST, COUNT(*) as total'))
+            ->join('estaciones as r', 'reservas.id_estacion_retiro', "=", 'r.id_estacion')
+            ->join('estaciones as d', 'reservas.id_estacion_devolucion', "=", 'd.id_estacion')
+            ->whereBetween(DB::raw('DATE(fecha_hora_devolucion)'), [$fechaInicio, $fechaFin])
+            ->groupBy('id_estacion_retiro', 'id_estacion_devolucion', 'r.nombre' ,'d.nombre')
+            ->orderBy('total', 'desc')
+            ->where('reservas.id_estado', 3)
+            ->get();
+
+        }else {
+
+            $rutas = [];
+
+        }
+        return view('informes.rutasInforme', compact('rutas'));
     }
     
 }
