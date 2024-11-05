a84e078f42fb9bd562efd1b754e6f5c806abeb54
Joako23L <joako_23402@hotmail.com>
2024-11-05 01:51:46 -0300

    Add: Le agrege unos minimos cambios para mejorar un poco la visibilidad de la view.

--- resources/views/cliente/ModificarReserva.blade.php
+++ resources/views/cliente/ModificarReserva.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <div class="container mx-auto mt-6">
-    <h3 class="text-3xl font-bold mb-4 text-center text-black">Modificar Reserva</h3>
+    <h3 class="text-3xl font-bold mb-4 text-center text-black title-section">Modificar Reserva</h3>
     <p>La estación seleccionada no tiene bicicletas disponibles. 
         ¿Desea modificar la reserva?</p>
     
-    <div class="bg-sky-100 p-6 rounded shadow-md">
+    <div class="bg-sky-100 p-6 rounded shadow-md mb-6 text-center">
         <p><strong>Nueva Estación de Retiro:</strong> {{ $nuevaEstacion->nombre}}</p>
         <p><strong>Nueva Hora de Retiro:</strong> {{ $nuevoHoraRetiro }}</p>
         <p><strong>Nueva Bicicleta Asignada:</strong> {{ $nuevaBicicleta->patente}}</p>
     </div>
-    
+    <div class="flex gap-4 justify-center">
     <form method="POST" action="{{ route('reservar.confirmarModificacion') }}">
         @csrf
         <input type="hidden" name="id_reserva" value="{{ $reserva->id_reserva }}">
@@ -24,6 +24,7 @@
         @csrf
         <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Rechazar Reserva</button>
     </form>
+    </div>
 </div>
 @endsection
 
