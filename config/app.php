6e77192b2e7516c356f2067d56d58e7480b5fb95
Daria Sauco <dsauco36@gmail.com>
2024-11-10 16:36:23 -0300

    fix: app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://pedalea-comodoro'),
+    'url' => env('APP_URL', 'http://pedalea-comodoro.local'),
 
     /*
     |--------------------------------------------------------------------------
