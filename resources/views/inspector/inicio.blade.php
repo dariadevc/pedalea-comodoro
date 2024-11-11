9c6d5c645ad3d700219f435cd4989b4f7280336e
Maxi Mansilla <maximansilla902@gmail.com>
2024-11-11 19:17:52 -0300

    botones clickeables

--- resources/views/inspector/inicio.blade.php
+++ resources/views/inspector/inicio.blade.php
@@ -10,12 +10,12 @@
 </div>
 <div class="grid grid-cols-2 gap-6">
     {{-- CONSULTAR SALDO --}}
-    <div class=" bg-gray-50 w-full h-36 p-4 shadow-md rounded-xl flex flex-col items-center gap-1" onclick="window.location.href='{{ route('inspector.bicicletas') }}'">
+    <a class=" bg-gray-50 w-full h-36 p-4 shadow-md rounded-xl flex flex-col items-center gap-1" href="{{ route('inspector.bicicletas') }}">
         <img src="img/bicicleta.png" alt="" class="h-14 w-14">
         <h3 class="text-center text-pc-texto-h semibol">Deshabilitar las bicicletas</h3>
-    </div>
+    </a>
     {{-- VER MAPA --}}
-    <div class=" bg-gray-50 w-full h-36 p-4 shadow-md rounded-xl flex flex-col items-center gap-1" onclick="window.location.href='{{ route('inspector.infraccion') }}'">
+    <a class=" bg-gray-50 w-full h-36 p-4 shadow-md rounded-xl flex flex-col items-center gap-1" href="{{ route('inspector.infraccion') }}">
         <svg width="50px" height="50px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" class="mt-1">
             <path d="M22.0098 12.39V7.39001C22.0098 6.32915 21.5883 5.31167 20.8382 4.56152C20.0881 3.81138 19.0706 3.39001 18.0098 3.39001H6.00977C4.9489 3.39001 3.93148 3.81138 3.18134 4.56152C2.43119 5.31167 2.00977 6.32915 2.00977 7.39001V17.39C2.00977 18.4509 2.43119 19.4682 3.18134 20.2184C3.93148 20.9685 4.9489 21.39 6.00977 21.39H12.0098"
                 stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -27,6 +27,6 @@
                 stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
         <h3 class="text-center">Generar infracciones</h3>
-    </div>
+    </a>
 </div>
 @endsection
