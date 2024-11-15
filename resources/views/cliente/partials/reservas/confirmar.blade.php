fe7f3354a2d4672e83ff22f94830bcfecb06ae1c
Daria Sauco <dsauco36@gmail.com>
2024-11-15 00:33:02 -0300

    fix: botones cancelar en rojo

--- resources/views/cliente/partials/reservas/confirmar.blade.php
+++ resources/views/cliente/partials/reservas/confirmar.blade.php
@@ -1,52 +1,59 @@
 <div id="contenedorReserva" class="flex flex-col lg:flex-row gap-6 mt-5 w-full">
-    <div class="">
-        <p id="parrafoInformacion" class="text-pc-texto-h text-base font-bold border-l-4 border-l-pc-rojo pl-2">
+    <div class="w-full">
+        <p id="parrafoInformacion" class="text-pc-texto-h text-base font-bold border-l-4 border-l-pc-rojo pl-2 mb-4">
             Datos de la reserva
         </p>
+        <div id="contenedorDatosReserva"
+            class="bg-gradient-to-br from-pc-naranja to-pc-rojo w-full p-4 shadow-md rounded-xl flex flex-col gap-6">
+            <!-- Información de la reserva -->
 
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación
+                    de
+                    retiro</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['estacion_retiro_nombre'] }}</p>
+            </div>
+
+
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label
+                    class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación
+                    de
+                    devolución</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['estacion_devolucion_nombre'] }}</p>
+            </div>
+
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Horario
+                    de
+                    retiro</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['horario_retiro'] }}</p>
+            </div>
+
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Tiempo
+                    de
+                    uso</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['tiempo_uso'] }} horas</p>
+            </div>
+
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Monto
+                    total
+                    de
+                    la reserva</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">${{ $reserva['monto_total'] }}</p>
+            </div>
+
+            <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
+                <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Monto
+                    de la
+                    seña a pagar</label>
+                <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">${{ $reserva['monto_senia'] }}</p>
+            </div>
+        </div>
     </div>
-    <div id="contenedorDatosReserva"
-        class="bg-gradient-to-br from-pc-naranja to-pc-rojo w-full lg:w-2/3 p-4 shadow-md rounded-xl flex flex-col gap-6">
-        <!-- Información de la reserva -->
 
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación de
-                retiro</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['estacion_retiro_nombre'] }}</p>
-        </div>
-
-
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación de
-                devolución</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['estacion_devolucion_nombre'] }}</p>
-        </div>
-
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Horario de
-                retiro</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['horario_retiro'] }}</p>
-        </div>
-
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Tiempo de
-                uso</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">{{ $reserva['tiempo_uso'] }} horas</p>
-        </div>
-
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Monto total
-                de
-                la reserva</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">${{ $reserva['monto_total'] }}</p>
-        </div>
-
-        <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-            <label class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Monto de la
-                seña a pagar</label>
-            <p class="bg-gray-100 border-b-2 text-pc-texto-p p-2">${{ $reserva['monto_senia'] }}</p>
-        </div>
-    </div>
 
     <div class="flex flex-col lg:w-1/3 gap-4 justify-center">
         <p class="text-pc-texto-p text-base border-l-4 border-l-pc-rojo pl-2">¿Los datos de la reserva son correctos?
--- resources/views/cliente/partials/reservas/elegir-datos.blade.php
+++ resources/views/cliente/partials/reservas/elegir-datos.blade.php
@@ -1,7 +1,7 @@
-
 <div id="contenedorFormDatosReserva" class="flex flex-col lg:flex-row gap-6 mt-5 w-full">
     <!-- Formulario de reserva -->
-    <div class="bg-gradient-to-br from-pc-naranja to-pc-rojo w-full lg:w-2/3 p-4 shadow-md rounded-xl flex flex-col gap-6">
+    <div
+        class="bg-gradient-to-br from-pc-naranja to-pc-rojo w-full lg:w-2/3 p-4 shadow-md rounded-xl flex flex-col gap-6">
         <form id="formularioHorarioRetiro" action="{{ route('estaciones.disponibilidad-horario-retiro') }}" method="POST"
             class="shadow-md rounded-xl flex flex-col gap-6">
             @csrf
@@ -9,10 +9,14 @@
             <input type="hidden" name="paso" id="paso">
 
             <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-                <label for="horario_retiro" class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Horario de Retiro</label>
+                <label for="horario_retiro"
+                    class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Horario de
+                    Retiro</label>
                 <input type="time" id="horario_retiro" name="horario_retiro"
-                    class="bg-gray-50 border-b-2 text-pc-texto-p w-full sm:max-w-xs" onchange="traerEstacionesDisponibles()">
-                <div id="error-horario-retiro" class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
+                    class="bg-gray-50 border-b-2 text-pc-texto-p w-full sm:max-w-xs"
+                    onchange="traerEstacionesDisponibles()">
+                <div id="error-horario-retiro"
+                    class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
             </div>
         </form>
 
@@ -23,21 +27,33 @@
             <input type="hidden" id="horario_retiro_reserva" name="horario_retiro_reserva" value="">
 
             <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-                <label for="estacion_retiro" class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación de Retiro</label>
-                <select id="estacion_retiro" name="estacion_retiro" class="bg-gray-50 border-b-2 text-pc-texto-p" disabled></select>
-                <div id="error-estacion-retiro" class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
+                <label for="estacion_retiro"
+                    class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación
+                    de Retiro</label>
+                <select id="estacion_retiro" name="estacion_retiro" class="bg-gray-50 border-b-2 text-pc-texto-p"
+                    disabled></select>
+                <div id="error-estacion-retiro"
+                    class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
             </div>
 
             <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-                <label for="estacion_devolucion" class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación de Devolución</label>
-                <select id="estacion_devolucion" name="estacion_devolucion" class="bg-gray-50 border-b-2 text-pc-texto-p" disabled></select>
-                <div id="error-estacion-devolucion" class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
+                <label for="estacion_devolucion"
+                    class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Estación
+                    de Devolución</label>
+                <select id="estacion_devolucion" name="estacion_devolucion"
+                    class="bg-gray-50 border-b-2 text-pc-texto-p" disabled></select>
+                <div id="error-estacion-devolucion"
+                    class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
             </div>
 
             <div class="flex flex-col gap-2 py-2 px-4 bg-gray-50 rounded-xl shadow-sm">
-                <label for="tiempo_uso" class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Tiempo de uso</label>
-                <select id="tiempo_uso" name="tiempo_uso" class="bg-gray-50 border-b-2 text-pc-texto-p" disabled></select>
-                <div id="error-tiempo-uso" class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
+                <label for="tiempo_uso"
+                    class="font-semibold text-wrap text-pc-texto-h border-b-2 border-b-pc-texto-h self-start">Tiempo de
+                    uso</label>
+                <select id="tiempo_uso" name="tiempo_uso" class="bg-gray-50 border-b-2 text-pc-texto-p"
+                    disabled></select>
+                <div id="error-tiempo-uso"
+                    class="error-message text-black-500 text-sm border-l-4 border-l-pc-rojo pl-2 disabled"></div>
             </div>
         </form>
     </div>
